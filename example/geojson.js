3db0430f806726b4bcef86d5293714a52cdc7067
hero <hero@asterisk-works.jp>
2016-06-25 06:12:28 +0900

    add wrapMapShaperRunCommands, and refactoring

--- example/geojson.js
+++ example/geojson.js
@@ -1,4 +1,4 @@
 var JG = require("../index.js").japan_geojson
 //JG.japanDetailGeojson()
- JG.japanGeojson()
-// JG.japanPrefsGeojson('./dest/geojson/00_japan.geojson', 'geojson')
+JG.japanGeojson()
+JG.japanPrefsGeojson('./dest/geojson/00_japan.geojson', 'geojson')
--- example/topojson.js
+++ example/topojson.js
@@ -1,4 +1,5 @@
 var JG = require("../index.js").japan_geojson
 // JG.japanDetailGeojson()
 // JG.japanGeojson()
+JG.geo2topo('dest/geojson/00_japan.geojson','dest/topojson/00_japan.topojson')
 JG.japanPrefsGeojson('dest/geojson/00_japan.geojson', 'topojson')
