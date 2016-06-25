b367b22c851e833d309a7a99a3652b0a2435ff64
hero <hero@asterisk-works.jp>
2016-06-25 16:11:52 +0900

    add unzip util

--- example/geojson.js
+++ example/geojson.js
@@ -1,4 +1,11 @@
+"use strict"
+const src_file = './src_data/N03-150101_GML.zip'
 var JG = require("../index.js").japan_geojson
-//JG.japanDetailGeojson()
-JG.japanGeojson()
+var utils = require("../index.js").utils
+
+utils.unzip(src_file)
+.then(function(shp_file){
+    //JG.japanDetailGeojson()
+    JG.japanGeojson(shp_file)
+})
 JG.japanPrefsGeojson('./dest/geojson/00_japan.geojson', 'geojson')
--- example/topojson.js
+++ example/topojson.js
@@ -1,5 +1,12 @@
+"use strinct"
+const src_file = './src_data/N03-150101_GML.zip'
 var JG = require("../index.js").japan_geojson
-// JG.japanDetailGeojson()
-// JG.japanGeojson()
+var utils = require("../index.js").utils
+
+utils.unzip(src_file)
+.then(function(shp_file){
+    // JG.japanDetailGeojson()
+    JG.japanGeojson(shp_file)
+})
 JG.geo2topo('dest/geojson/00_japan.geojson','dest/topojson/00_japan.topojson')
 JG.japanPrefsGeojson('dest/geojson/00_japan.geojson', 'topojson')
