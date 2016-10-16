2f0c09dd14419b743f1b1241012a3d9ce7ed3afd
HeRoMo <hero@asterisk-works.jp>
2016-10-16 18:11:18 +0900

    update data to 160101

--- example/topojson.js
+++ example/topojson.js
@@ -1,12 +1,13 @@
 "use strinct"
-const src_file = './src_data/N03-150101_GML.zip'
+const src_file = './src_data/N03-160101_GML.zip'
 var JG = require("../index.js").japan_geojson
 var utils = require("../index.js").utils
 
 utils.unzip(src_file)
 .then(function(shp_file){
-    // JG.japanDetailGeojson()
+    JG.japanDetailGeojson(shp_file)
     JG.japanGeojson(shp_file)
 })
+JG.geo2topo('dest/geojson/00_japan_detail.geojson','dest/topojson/00_japan_detail.topojson')
 JG.geo2topo('dest/geojson/00_japan.geojson','dest/topojson/00_japan.topojson')
 JG.japanPrefsGeojson('dest/geojson/00_japan.geojson', 'topojson')
