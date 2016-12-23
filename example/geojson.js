adfc5fc6f02ba39157c91eeb50aa7c8070f6550a
HeRoMo <hero@asterisk-works.jp>
2016-12-23 19:17:10 +0900

    add japanAllPrefsGeojson

--- example/geojson.js
+++ example/geojson.js
@@ -7,5 +7,6 @@ utils.unzip(src_file)
 .then(function(shp_file){
     JG.japanDetailGeojson(shp_file)
     JG.japanGeojson(shp_file)
+    JG.japanAllPrefsGeojson(shp_file)
 })
 JG.japanPrefsGeojson('./dest/geojson/00_japan.geojson', 'geojson')
