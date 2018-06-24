81b7aba51930ec579e78375c298a167b19ecd049
HeRoMo <hero@asterisk-works.jp>
2018-06-24 21:08:02 +0900

    fix bug

--- example/topojson.js
+++ example/topojson.js
@@ -3,9 +3,10 @@ const JG = require('../index.js').japanGeojson;
 
 const convertToGeojson = require('./geojson');
 
-(async () => { await convertToGeojson(srcFile); })();
-
-JG.geo2topo('dest/geojson/00_japan_detail.geojson', 'dest/topojson/00_japan_detail.topojson');
-JG.geo2topo('dest/geojson/00_japan_prefs.geojson', 'dest/topojson/00_japan_prefs.topojson');
-JG.geo2topo('dest/geojson/00_japan.geojson', 'dest/topojson/00_japan.topojson');
-JG.japanPrefsGeojson('dest/geojson/00_japan.geojson', 'topojson');
+(async () => {
+  await convertToGeojson(srcFile);
+  JG.geo2topo('dest/geojson/00_japan_detail.geojson', 'dest/topojson/00_japan_detail.topojson');
+  JG.geo2topo('dest/geojson/00_japan_prefs.geojson', 'dest/topojson/00_japan_prefs.topojson');
+  JG.geo2topo('dest/geojson/00_japan.geojson', 'dest/topojson/00_japan.topojson');
+  JG.japanPrefsGeojson('dest/geojson/00_japan.geojson', 'topojson');
+})();
