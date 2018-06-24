034f98c18feb2f9873fbeca120156e0e9070e0a4
HeRoMo <hero@asterisk-works.jp>
2018-06-24 18:43:44 +0900

    use async/await

--- example/geojson.js
+++ example/geojson.js
@@ -2,10 +2,14 @@ const srcFile = './src_data/N03-160101_GML.zip';
 const JG = require('../index.js').japanGeojson;
 const { utils } = require('../index.js');
 
-utils.unzip(srcFile)
-  .then((shpFile) => {
-    JG.japanDetailGeojson(shpFile);
-    JG.japanGeojson(shpFile);
-    JG.japanAllPrefsGeojson(shpFile);
-  });
-JG.japanPrefsGeojson('./dest/geojson/00_japan.geojson', 'geojson');
+async function convertToGeojson() {
+  const shpFile = await utils.unzip(srcFile);
+  await Promise.all([
+    JG.japanDetailGeojson(shpFile),
+    JG.japanGeojson(shpFile),
+    JG.japanAllPrefsGeojson(shpFile),
+  ]);
+  JG.japanPrefsGeojson('./dest/geojson/00_japan.geojson', 'geojson');
+}
+
+convertToGeojson();
